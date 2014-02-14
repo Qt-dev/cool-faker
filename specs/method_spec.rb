2052e95285eb378fa2ffc1183127d06640c9d93d
Jamie & Quentin Devauchelle <pair+jamck23.qt-dev@devbootcamp.com>
2014-02-14 15:19:47 -0800

    Added quote method and tests - passes all tests - booyah

--- specs/method_spec.rb
+++ specs/method_spec.rb
@@ -1,13 +1,32 @@
 require 'rspec'
-require_relative '../lib/name.rb'
+# require_relative '../lib/name.rb'
+require_relative '../lib/cool_faker.rb'
+
 
 describe CoolFaker::Character, "name class for famous faker" do
 
   describe "name method" do
-  name = CoolFaker::Character
     it "should return a string" do
-      name.name.should be_a String
+      CoolFaker::Character.name.should be_a String
     end
+  end
+
+end
+
+describe CoolFaker::Team, "team class for famous faker" do
+  describe "team slogan method" do
+    it "should return a string" do
+      CoolFaker::Team.slogan.should be_a String
+    end
+
+    it "should be shorter than 140 characters" do
+      CoolFaker::Team.slogan.length.should be < 140
+    end
+
+    it "should be longer than 3 words" do
+      CoolFaker::Team.slogan.split(" ").length.should be > 3
+    end
+
   end
 
 end
